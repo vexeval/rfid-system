c2c8aee203162ac33753b9236d4ee12299ae78c8
Matt <90877660+vexeval@users.noreply.github.com>
2022-02-15 14:27:42 -0500

    Initial File

---
+++ activate.py
@@ -0,0 +1,14 @@
+
+import Rpi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+text = input("New Data: ")
+print("Place tag")
+
+try:
+  reader.write(text)
+  print("Written")
+finally:
+  GPIO.cleanup()
